2ecac192d407b7446b12f2e7ae9b65866106de75
kerolesnabiel <keronabilzaki@gmail.com>
2025-04-23 16:56:51 +0200

    Add Navbar component and update HTML metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="min-h-screen">
+      <Navbar />
       <Banner />
     </div>
   );
